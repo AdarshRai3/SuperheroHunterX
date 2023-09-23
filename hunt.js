446940a128f1ca4bac6bd3eb83784ded3519fd31
AdarshRai3 <git config --global user.email>
2023-09-23 09:05:56 +0530

    18

--- hunt.js
+++ hunt.js
@@ -29,21 +29,29 @@ input.addEventListener("keyup", async () => {
 
   const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${input.value}`;
 
-  const response = await fetch(url);
-  const jsonData = await response.json();
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const jsonData = await response.json();
 
-  jsonData.data["results"].forEach((result) => {
-    let name = result.name;
-    let div = document.createElement("div");
-    div.style.cursor = "pointer";
-    div.classList.add("autocomplete-items");
-    div.setAttribute("onclick", "displayWords('" + name + "')");
-    let word = "<b>" + name.substr(0, input.value.length) + "</b>";
-    word += name.substr(input.value.length);
-    div.innerHTML = `<p class="item">${word}</p>`;
-    listContainer.appendChild(div);
-  });
+    jsonData.data["results"].forEach((result) => {
+      let name = result.name;
+      let div = document.createElement("div");
+      div.style.cursor = "pointer";
+      div.classList.add("autocomplete-items");
+      div.setAttribute("onclick", "displayWords('" + name + "')");
+      let word = "<b>" + name.substr(0, input.value.length) + "</b>";
+      word += name.substr(input.value.length);
+      div.innerHTML = `<p class="item">${word}</p>`;
+      listContainer.appendChild(div);
+    });
+  } catch (error) {
+    console.log('Fetch failed: ', error);
+  }
 });
+
 
 button.addEventListener(
     "click",
